fb127bda036c3ebdb8483ee9b21b8f9c89576b33
KongWiki <kongwiki@163.com>
2020-02-24 15:59:18 +0800

    数组相关题目完成

--- src/main/java/com/wkk/array/MoveZeroes.java
+++ src/main/java/com/wkk/array/MoveZeroes.java
@@ -6,4 +6,41 @@ package com.wkk.array;
  * @Email: kongwiki@163.com
  */
 public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+    public static void moveZeroesII(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        MoveZeroes.moveZeroes(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
 }
