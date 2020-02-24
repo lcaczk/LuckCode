a582cd8c2caf1923d80ffaedd6f73ddab5e39aac
KongWiki <kongwiki@163.com>
2020-02-24 15:59:34 +0800

    添加树相关题目

--- src/main/java/com/wkk/tree/TreeNode.java
+++ src/main/java/com/wkk/tree/TreeNode.java
@@ -1,9 +1,17 @@
 package com.wkk.tree;
 
+import sun.reflect.generics.tree.Tree;
+
 /**
  * @Time: 20-2-24上午11:08
  * @Author: kongwiki
  * @Email: kongwiki@163.com
  */
 public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int val){
+        this.val = val;
+    }
+    public TreeNode(){}
 }
