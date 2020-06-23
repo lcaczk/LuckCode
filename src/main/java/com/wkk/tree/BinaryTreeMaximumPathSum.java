b2080e9ddc77a7aad6c7a66ffc6b61924a20ddb7
KongWiki <kongwiki@163.com>
2020-06-23 20:26:28 +0800

    update

--- src/main/java/com/wkk/tree/BinaryTreeMaximumPathSum.java
+++ src/main/java/com/wkk/tree/BinaryTreeMaximumPathSum.java
@@ -27,11 +27,20 @@ public class BinaryTreeMaximumPathSum {
     }
 
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
+        TreeNode node1 = new TreeNode(-10);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+
+        TreeNode node = new TreeNode(-2);
+        TreeNode node6 = new TreeNode(1);
+        node.left= node6;
+
         node1.left = node2;
         node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
 
         int max = maxPathSum(node1);
         System.out.println(max);
