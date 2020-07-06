3ca3a10ac7a6cbd8d1e9cc74ea938abac9e1f828
KongWiki <kongwiki@163.com>
2020-07-06 19:40:47 +0800

    每日一题

---
+++ src/main/java/com/wkk/everyday/july/UniquePathsII.java
@@ -0,0 +1,10 @@
+package com.wkk.everyday.july;
+
+/**
+ * @Time: 2020/7/6上午9:01
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class UniquePathsII {
+
+}
--- src/main/java/com/wkk/everyday/july/WildcardMatching.java
+++ src/main/java/com/wkk/everyday/july/WildcardMatching.java
@@ -13,23 +13,70 @@ public class WildcardMatching {
         return Pattern.matches(p, s);
     }
 
-    public static boolean isMatchII(String s, String p){
+    public static boolean isMatchII(String s, String p) {
         int m = s.length();
         int n = p.length();
-        boolean[][] dp = new boolean[m+1][n+1];
-        for (int i = 0; i < s.length(); i++) {
-            
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        // 仅做p的第一个判断 为* 则dp[0][1] = true
+        // 否则 dp[0][1] = false;
+        // 不适用直接判断 主要是防止过多的基础判断
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i-1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
         }
 
-
-
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else if(p.charAt(j-1) == '?' || p.charAt(j-1) == s.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+            }
+        }
+        System.out.println("sdds");
         return dp[m][n];
+
+
+    }
+
+    // 错误
+    public static boolean isMatchIII(String s, String p){
+        int m = s.length();
+        int n = p.length();
+        boolean[] dp = new boolean[n+1];
+        for (int i = 1; i <= n; i++) {
+            if(p.charAt(i-1) == '*'){
+                dp[i] = true;
+            }else {
+                break;
+            }
+        }
+
+        dp[0] = true;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(p.charAt(j-1) == '*'){
+                    dp[j] = dp[j] || dp[j-1];
+                }
+                else if(p.charAt(j-1) == '?' || p.charAt(j-1) == s.charAt(i-1)){
+                    dp[j] = dp[j-1];
+                }
+            }
+        }
+        return dp[n];
+
     }
 
     public static void main(String[] args) {
         String s = "aa";
         String p = "a";
-        boolean match = isMatch(s, p);
+        boolean match = isMatchIII(s, p);
         System.out.println(match);
     }
 }
