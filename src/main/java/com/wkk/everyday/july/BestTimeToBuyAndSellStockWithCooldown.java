8073323e0c866f7d1c93de0e0b0a2905eb577537
Weikunkun <kongwiki@163.com>
2020-07-10 20:32:37 +0800

    每日yiti

---
+++ src/main/java/com/wkk/everyday/july/BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,33 @@
+package com.wkk.everyday.july;
+
+import sun.applet.AppletResourceLoader;
+
+/**
+ * @Time: 2020/7/10上午9:53
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class BestTimeToBuyAndSellStockWithCooldown {
+    public static int maxProfit(int[] prices) {
+        if(prices.length<2 || prices == null){
+            return 0;
+        }
+        int n = prices.length;
+        int[][] f = new int[n][3];
+        f[0][0] = -prices[0];
+        f[1][0] = 0;
+        for (int i = 1; i < n; i++) {
+            f[i][0] = Math.max(f[i-1][0], f[i-1][2] - prices[i]);
+            f[i][1] = f[i-1][0] + prices[i];
+            f[i][2] = Math.max(f[i-1][1], f[i-1][2]);
+        }
+        return Math.max(f[n-1][1], f[n-1][2]);
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 0, 2};
+        int i = maxProfit(a);
+        System.out.println(i);
+    }
+}
