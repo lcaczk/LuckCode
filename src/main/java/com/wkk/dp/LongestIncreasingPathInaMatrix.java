c665a65c8cae6239b243a679e61675aca58a0cf3
Weikunkun <kongwiki@163.com>
2020-07-26 16:26:50 +0800

    upate

---
+++ src/main/java/com/wkk/dp/LongestIncreasingPathInaMatrix.java
@@ -0,0 +1,9 @@
+package com.wkk.dp;
+
+/**
+ * @Time: 2020/7/26上午8:52
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class LongestIncreasingPathInaMatrix {
+}
---
+++ src/main/java/com/wkk/everyday/july/LongestIncreasingPathInaMatrix.java
@@ -0,0 +1,47 @@
+package com.wkk.everyday.july;
+
+/**
+ * @Time: 2020/7/26上午8:51
+ * @Author: kongwiki
+ * @Email: kongwiki@163.com
+ */
+public class LongestIncreasingPathInaMatrix {
+    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int rows, columns;
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        rows = matrix.length;
+        columns = matrix[0].length;
+        int[][] memo = new int[rows][columns];
+        int ans = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                ans = Math.max(ans, dfs(matrix, i, j, memo));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+        if (memo[row][column] != 0) {
+            return memo[row][column];
+        }
+        ++memo[row][column];
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newColumn = column + dir[1];
+            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+            }
+        }
+        return memo[row][column];
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {};
+        System.out.println(a.length);
+        System.out.println(a[0].length);
+    }
+}
