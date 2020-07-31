784fc0df4d4ea4e573b1f1b0c5319f9645450720
Weikunkun <kongwiki@163.com>
2020-07-31 10:51:58 +0800

    update

---
+++ src/main/java/com/wkk/array/JumpGame.java
@@ -0,0 +1,8 @@
+package com.wkk.array;
+
+/**
+ * @author kongwiki@163.com
+ * @since 2020/7/30下午8:57
+ */
+public class JumpGame {
+}
---
+++ src/main/java/com/wkk/everyday/july/MagicIndexLCCI.java
@@ -0,0 +1,8 @@
+package com.wkk.everyday.july;
+
+/**
+ * @author kongwiki@163.com
+ * @since 2020/7/31上午8:26
+ */
+public class MagicIndexLCCI {
+}
