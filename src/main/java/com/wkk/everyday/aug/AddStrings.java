6909792b04a54098e1d8a4fa59dda681cf0981ee
Weikunkun <kongwiki@163.com>
2020-08-03 09:46:27 +0800

    每日一题

---
+++ src/main/java/com/wkk/everyday/aug/AddStrings.java
@@ -0,0 +1,32 @@
+package com.wkk.everyday.aug;
+
+/**
+ * @author kongwiki@163.com
+ * @since 2020/8/3上午9:18
+ */
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1 ;
+        int j = num2.length() - 1;
+        while(i >= 0 || j >= 0){
+            int a =(i >= 0) ?  num1.charAt(i) - '0': 0;
+            int b =(j >= 0) ? num2.charAt(j) - '0': 0;
+            int temp = (a + b + carry);
+            sb.append(temp%10);
+            carry = temp/10;
+            i--;
+            j--;
+        }
+        if(carry > 0){
+            sb.append(carry);
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a = "123";
+        String b = "4564";
+    }
+}
