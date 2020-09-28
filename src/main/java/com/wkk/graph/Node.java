77184b103f5745b52d96716bee405d74b615e280
Weikunkun <kongwiki@163.com>
2020-09-28 20:42:19 +0800

    add dailyQuestion#117

--- src/main/java/com/wkk/graph/Node.java
+++ src/main/java/com/wkk/graph/Node.java
@@ -9,6 +9,9 @@ import java.util.List;
  */
 public class Node {
     public int val;
+    public Node left;
+    public Node right;
+    public Node next;
     public List<Node> neighbors;
 
     public Node() {
@@ -24,5 +27,12 @@ public class Node {
     public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
     }
 }
