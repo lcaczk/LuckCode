0e56be9410fb04025870c0aa0371a4a0535a3f3f
StoneHell <stormstone8@qq.com>
2020-10-22 20:49:57 +0800

    :sparkles: update common

--- src/main/java/com/common/structure/ListNode.java
+++ src/main/java/com/common/structure/ListNode.java
@@ -1,16 +1,48 @@
 package com.common.structure;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 /**
  * @Time: 20-2-22下午4:12
  * @Author: kongwiki
  * @Email: kongwiki@163.com
  */
 public class ListNode {
+
     public int val;
     public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
     }
-    public ListNode(){}
+
+    public ListNode() {
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+    public static ListNode numsToListNode(int[] nums) {
+        ListNode head = new ListNode(-1);
+        ListNode p = head;
+        for (int n : nums) {
+            p.next = new ListNode(n);
+            p = p.next;
+        }
+        return head.next;
+    }
+
+    public static void printListNode(ListNode head) {
+        LinkedList<Integer> res = new LinkedList<>();
+        ListNode p = head;
+        while (!Objects.isNull(p)) {
+            res.addLast(p.val);
+            p = p.next;
+        }
+        System.out.println(res.toString());
+    }
 }
--- src/main/java/com/common/structure/TreeNode.java
+++ src/main/java/com/common/structure/TreeNode.java
@@ -1,5 +1,8 @@
 package com.common.structure;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Time: 20-2-24上午11:08
  * @Author: kongwiki
@@ -8,10 +11,56 @@ package com.common.structure;
 public class TreeNode {
     public int val;
     public int count;
-    public TreeNode left, right;
-    public TreeNode(int val){
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
         this.val = val;
         this.count = 0;
     }
-    public TreeNode(){}
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * 根据序列（层序）构建二叉树
+     */
+    public static TreeNode createBinaryTree(List<Integer> list) {
+        TreeNode root = new TreeNode(list.get(0));
+        //层序创建二叉树
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        TreeNode current = null;
+        Integer value = null;
+        while (i < list.size()) {
+            // 从链表中移除并获取第一个节点
+            current = queue.poll();
+            value = list.get(i++);
+            if (value != null && current != null) {
+                TreeNode left = new TreeNode(value);
+                // 创建当前节点的左孩子
+                current.left = left;
+                // 在链表尾部 左孩子入队
+                queue.offer(left);
+            }
+            value = list.get(i++);
+            if (value != null && current != null) {
+                TreeNode right = new TreeNode(value);
+                // 创建当前节点的右孩子
+                current.right = right;
+                // 在链表尾部 右孩子入队
+                queue.offer(right);
+            }
+
+        }
+        return root;
+    }
+
 }
