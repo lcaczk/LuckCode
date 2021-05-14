feb1bc49b6d7de8479cb3e1f8460d5a18be8ecc2
weikunkun <kongwiki@163.com>
2021-05-14 17:28:33 +0800

    add something

--- src/main/java/com/wkk/dp/twosequences/LC_72.java
+++ src/main/java/com/wkk/dp/twosequences/LC_72.java
@@ -20,6 +20,7 @@ public class LC_72 {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 // 因为dp数组有效位从1开始
+
                 // 所以当前遍历到的字符串的位置为i-1 | j-1
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
